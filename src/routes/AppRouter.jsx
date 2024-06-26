76c5ffa7b6b0b82f8dc42b031be14404fc71e3f9
BossNPtl <boss.dowork@gmail.com>
2024-06-26 18:06:18 +0700

    create navbar

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -5,9 +5,17 @@ import MainContainer from "../layouts/MainContainer";
 import VerifyPage from "../pages/VerifyPage";
 import WaitingApprovePage from "../pages/WaitingApprovePage";
 import RiderHomePage from "../pages/RiderHomePage";
+import RegisterAndLoginPage from "../pages/RegisterAndLoginPage";
 
 const router = createBrowserRouter([
-
+    {
+        path: "/auth/register",
+        element: (
+            <div className="w-lvw h-lvh bg-black flex justify-center">
+                <RegisterAndLoginPage />
+            </div>
+        ),
+    },
     {
         path: "/",
         element: (
