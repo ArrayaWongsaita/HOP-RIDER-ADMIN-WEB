369cfe741df4f12bb6a88bac22c19788cd15cabd
STEViely <stevielyfiles@gmail.com>
2024-07-08 17:55:05 +0700

    editeResponsive

--- src/pages/admin/CustomerController.jsx
+++ src/pages/admin/CustomerController.jsx
@@ -6,41 +6,46 @@ import { dataRider } from "../../constants/dataRider";
 import ComponentOrder from "../../components/ComponentOrder";
 
 export default function CustomerController() {
-  const [search, setSearch] = useState('');
-  const userRider = dataRider     // mock up
-  console.log(userRider)
+  const [search, setSearch] = useState("");
+  const userRider = dataRider; // mock up
+  console.log(userRider);
 
-  const targetRider = userRider.filter(item => (
-    (`${item.id}`).includes(search) || (item.firstName).toLowerCase().includes(search.toLowerCase())
-  ))
-  console.log('targetRider',targetRider)
+  const targetRider = userRider.filter(
+    (item) =>
+      `${item.id}`.includes(search) ||
+      item.firstName.toLowerCase().includes(search.toLowerCase())
+  );
+  console.log("targetRider", targetRider);
   const handleOnChance = (event) => {
-    setSearch(event.target.value)
-  }
-  
+    setSearch(event.target.value);
+  };
+
   return (
-    <div>
-      <div>
-        <div className={`w-full h-[70px] flex items-center justify-center p-4 mb-5
+    <div className="w-full h-full">
+      <div
+        className={`w-full h-[12%] flex items-center justify-center 
+           mb-5
             font-semibold text-xl text-white
-            bg-gradient-to-r from-[#1D2B53] from-30% to-[#FF004D] to-100% `}>
-          <InputSearch
-            placeholder='search'
-            onChange={handleOnChance}
-            // onClick={() => console.log('Search di kub')}
-            name='search'
-            value={search}
-            rounded="xxlLeft"
-          />
-        </div>
+            bg-gradient-to-r from-[#1D2B53] from-30% to-[#FF004D] to-100% `}
+      >
+        <InputSearch
+          placeholder="search"
+          onChange={handleOnChance}
+          // onClick={() => console.log('Search di kub')}
+          name="search"
+          value={search}
+          rounded="xxlLeft"
+        />
       </div>
-      <div className="w-[90%] mx-auto flex flex-col gap-3">
-        {targetRider.length > 0 && targetRider.map((item) =>
-        <RiderBar key={item.id} data={item} >
-          <ComponentOrder data={item} />
-        </RiderBar>
-        )}
+
+      <div className="flex flex-col gap-3 ">
+        {targetRider.length > 0 &&
+          targetRider.map((item) => (
+            <RiderBar key={item.id} data={item}>
+              <ComponentOrder data={item} />
+            </RiderBar>
+          ))}
       </div>
     </div>
-  )
+  );
 }
